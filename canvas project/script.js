0beda43b90d593a38e7b2d9c092fdc2afbe4d995
oscar <159032472+odgar@users.noreply.github.com>
2024-04-25 09:17:23 -0400

    initialized

---
+++ canvas project/script.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById("canvas");
+
+var ctx = canvas.getContext("2d");
+
+
+
+
+
+
+
+
+
+/*
+canvas.addEventListener('mousedown', () => {
+    ctx.beginPath();
+    ctx.arc(250, 250, 150, 0, 2 * Math.PI)
+    ctx.stroke();
+})
+
+/*
+//rect = rectangle
+canvas.addEventListener('mouseup', () => {
+    ctx.clearRect(0,0,canvas.clientWidth, canvas.height);
+})
+
+/*
+//need to draw da rectangle
+ctx.lineTo(500, 190);
+ctx.lineTo(500, 400);
+ctx.lineTo(300, 400);
+ctx.lineTo(300,190);
+ctx.stroke(); 
+
+// triangle
+ctx.moveTo(400, 30);
+ctx.lineTo(400, 30);
+ctx.lineTo(500, 190);
+ctx.lineTo(300, 190);
+ctx.lineTo(400, 30);
+ctx.stroke();
+
+// door
+ctx.moveTo(435, 400);
+ctx.lineTo(435, 295);
+ctx.lineTo(435, 400);
+ctx.lineTo(435, 400);
+ctx.lineTo(365, 400);
+ctx.lineTo(365,295);
+ctx.lineTo(435,295);
+ctx.stroke(); */
